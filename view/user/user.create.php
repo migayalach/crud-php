abe7e622427538dd7d7d2caf9280ccd3cd557933
migayalach <ayalachavezmiguel@gmail.com>
2025-09-30 09:57:52 -0400

    created form

---
+++ view/user/user.create.php
@@ -0,0 +1,16 @@
+<?php
+  require_once("../head/head.php");
+?> 
+
+  <form action="store.php" method="post" autocomplete="off">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Name user</label>
+      <input type="text" name="name" class="form-control" id="name" aria-describedby="name">
+    </div>
+    <button type="submit" class="btn btn-primary">Save</button>
+    <a class="btn btn-danger" href="/crud/index.php">Cancel</a>
+  </form>
+
+<?php
+  require_once("../head/footer.php");
+?> 
