de8f64c808b30d4641f1c0f617bc6958a2e62125
migayalach <ayalachavezmiguel@gmail.com>
2025-10-01 23:42:52 -0400

    create new class

---
+++ view/user/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once("/home/miguel/Documents/htdocs/crud/view/head/head.php");
+require_once("/home/miguel/Documents/htdocs/crud/controller/user.controller.php");
+$obj = new UserController();
+?>
+
+<?php
+require_once("/home/miguel/Documents/htdocs/crud/view/head/footer.php");
+?>
