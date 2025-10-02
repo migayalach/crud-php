dcb0c07601ef0327f8a7836b8817e28bb9f07b8b
migayalach <ayalachavezmiguel@gmail.com>
2025-10-01 23:42:11 -0400

    added function show

--- model/user.model.php
+++ model/user.model.php
@@ -1,18 +1,32 @@
 <?php
-  class UserModel {
-      private $PDO;
+class UserModel
+{
+  private $PDO;
 
-      public function __construct(){
-          require_once("/home/miguel/Documents/htdocs/crud/configuration/connection.php");
-          $connection = new Connection();
-          $this->PDO = $connection->connect();
-      }
-
-      public function add($name){
-          $stament = $this->PDO->prepare("INSERT INTO username VALUES(null, :name)");
-          $stament->bindParam(":name", $name);
-          return ($stament->execute()) ? $this->PDO->lastInsertId() : false;
-      }
+  public function __construct()
+  {
+    require_once("/home/miguel/Documents/htdocs/crud/configuration/connection.php");
+    $connection = new Connection();
+    $this->PDO = $connection->connect();
   }
 
-?>
+  public function add($name)
+  {
+    $stament = $this->PDO->prepare("INSERT INTO username VALUES(null, :name)");
+    $stament->bindParam(":name", $name);
+    return ($stament->execute()) ? $this->PDO->lastInsertId() : false;
+  }
+
+  public function show($id)
+  {
+    $stament = $this->PDO->prepare("SELECT * FROM username where id = :id limit 1");
+    $stament->bindParam(":id", $id);
+    return ($stament->execute()) ? $stament->fetch() : false;
+  }
+
+  public function index()
+  {
+    $stament = $this->PDO->prepare("SELECT * FROM username");
+    return ($stament->execute()) ? $stament->fetchAll() : false;
+  }
+}
