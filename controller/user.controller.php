52f40ba1402e3a376308989224eaf2aed6138eb0
migayalach <ayalachavezmiguel@gmail.com>
2025-10-01 23:41:38 -0400

    added function index

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -1,15 +1,27 @@
-<?php 
-  class UserController{
-    private $model;
+<?php
+class UserController
+{
+  private $model;
 
-    public function __construct(){
-      require_once("/home/miguel/Documents/htdocs/crud/model/user.model.php");
-      $this->model = new UserModel();
-    }
-    
-    public function save($name){
-      $id = $this->model->add($name);
-      return ($id != false) ? header("Location:user.show.php?id=".$id) : header("Location:user.create.php");
-    }
+  public function __construct()
+  {
+    require_once("/home/miguel/Documents/htdocs/crud/model/user.model.php");
+    $this->model = new UserModel();
   }
-?>
+
+  public function save($name)
+  {
+    $id = $this->model->add($name);
+    return ($id != false) ? header("Location:user.show.php?id=" . $id) : header("Location:user.create.php");
+  }
+
+  public function show($id)
+  {
+    return ($this->model->show($id) != false) ? $this->model->show($id) : header("Location:index.php");
+  }
+
+  public function index()
+  {
+    return ($this->model->index()) ? $this->model->index() : false;
+  }
+}
